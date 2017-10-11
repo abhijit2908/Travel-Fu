8b7c250facdb413ed4549d5c506aca7a9768c222
Max Wang <maxwellswang@gmail.com>
2017-10-10 20:38:37 -0400

    added content to login panel and firebase.js

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBw2bE_pxCWSJM0zdkhUtouRJfqjGfZgxc",
+authDomain: "fir-campers.firebaseapp.com",
+databaseURL: "https://fir-campers.firebaseio.com",
+projectId: "fir-campers",
+storageBucket: "",
+messagingSenderId: "396436072298"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
